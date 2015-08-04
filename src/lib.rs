b39688a2765fa42338b22341803d2318d533b917
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-08-04 15:45:19 -0400

    Add an example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
 //! A constructor of SQL statements.
+//!
+//! ## Example
+//!
+//! ```
+//! use sql::prelude::*;
+//!
+//! // CREATE TABLE `users` (`id` INTEGER NOT NULL, `name` TEXT, `photo` BLOB)
+//! println!("{}", create_table("users").column(column("id").integer().not_null())
+//!                                     .column(column("name").string())
+//!                                     .column(column("photo").binary())
+//!                                     .compile().unwrap());
+//!
+//! // INSERT INTO `users` (`id`, `name`) VALUES (?, ?), (?, ?)
+//! println!("{}", insert_into("users").columns(&["id", "name"]).batch(2)
+//!                                    .compile().unwrap());
+//!
+//! // SELECT * FROM `users` WHERE `name` LIKE 'A%'
+//! println!("{}", select_from("users").so_that(column("name").like("A%"))
+//!                                    .compile().unwrap());
+//!
+//! // SELECT `name`, `photo` FROM `users` LIMIT 1
+//! println!("{}", select_from("users").columns(&["name", "photo"]).limit(1)
+//!                                    .compile().unwrap());
+//! ```
 
 use std::default::Default;
 use std::{error, fmt, result};
